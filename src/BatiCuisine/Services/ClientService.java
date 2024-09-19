4d6ea7da67785674e0de4b55d3035faa5b9b3e61
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-09-19 16:30:24 +0100

    modifier ClientService

--- src/BatiCuisine/Services/ClientService.java
+++ src/BatiCuisine/Services/ClientService.java
@@ -8,29 +8,32 @@ import java.util.List;
 import java.util.Optional;
 
 public class ClientService {
-  ClientRepository clientRepository = new ClientRepository();
+  ClientRepository clientRepository ;
 
-  public Client save(Client client) throws SQLException {
+    public ClientService() {
+        this.clientRepository = new ClientRepository();
+    }
+
+    public Client save(Client client){
+        Optional<Client> existingClient = clientRepository.findByName(client.getName());
+        if(existingClient.isPresent()){
+            return null;
+        }
       return clientRepository.save(client);
   }
-  public Optional<Client> findById(int id) throws SQLException {
+  public Optional<Client> findById(int id){
       return clientRepository.findById(id);
   }
-  public List<Client> findAll() throws SQLException {
+  public Optional<Client> findByName(String name){
+        return clientRepository.findByName(name);
+  }
+  public List<Client> findAll(){
       return clientRepository.findAll();
   }
-  public Optional<Client> update(Client client) throws SQLException {
-      Optional<Client> existingClient = findById(client.getId());
-      if (existingClient.isPresent()){
-          return Optional.of(client);
-      }
-      return Optional.empty();
+  public Client update(Client client){
+    return clientRepository.update(client);
   }
-  public Boolean delete(int id) throws SQLException {
-      Optional<Client> existingClient = findById(id);
-      if (existingClient.isPresent()){
-         clientRepository.delete(id);
-      }
-      return true;
+  public Boolean delete(int id) {
+      return clientRepository.delete(id);
   }
 }
