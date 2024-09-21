952353ed1c0387b389dd3e11ef2784ea6bda06a3
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-09-21 14:27:40 +0100

    ajouter methode findById en laborService

--- src/BatiCuisine/Services/LaborService.java
+++ src/BatiCuisine/Services/LaborService.java
@@ -3,9 +3,14 @@ package BatiCuisine.Services;
 import BatiCuisine.Entities.Labor;
 import BatiCuisine.Repository.Implementation.LaborRepository;
 
+import java.util.Optional;
+
 public class LaborService {
     LaborRepository laborRepository = new LaborRepository();
     public Labor save(Labor labor){
         return laborRepository.save(labor);
+    }
+    public Optional<Labor> findById(int id){
+        return laborRepository.findById(id);
     }
 }
