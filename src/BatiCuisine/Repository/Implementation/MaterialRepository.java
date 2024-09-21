25ebef3e59f38154fc21640609858c6a0a34298d
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-09-21 14:03:02 +0100

    ajouter les autres methodes en materialRepsitory

--- src/BatiCuisine/Repository/Implementation/MaterialRepository.java
+++ src/BatiCuisine/Repository/Implementation/MaterialRepository.java
@@ -1,10 +1,12 @@
 package BatiCuisine.Repository.Implementation;
 
 import BatiCuisine.Config.DataBaseConnection;
+import BatiCuisine.Entities.Labor;
 import BatiCuisine.Entities.Material;
 import BatiCuisine.Repository.Interface.MaterialInterface;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +26,7 @@ public class MaterialRepository implements MaterialInterface {
             preparedStatement.setDouble(4, material.getUnitCost());
             preparedStatement.setDouble(5, material.getQuantity());
             preparedStatement.setDouble(6, material.getTransportCost());
-            preparedStatement.setDouble(6, material.setCoefficientQuality());
+            preparedStatement.setDouble(7, material.getCoefficientQuality());
             preparedStatement.executeUpdate();
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
             if (generatedKeys.next()){
@@ -39,17 +41,68 @@ public class MaterialRepository implements MaterialInterface {
 
     @Override
     public Optional<Material> findById(int id) {
+        String query = "SELECT * FROM materials WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Material material = new Material();
+                material.setName(resultSet.getString("name"));
+                material.setVatRate(resultSet.getDouble("vatrate"));;
+                material.setUnitCost(resultSet.getDouble("unitcost"));
+                material.setQuantity(resultSet.getDouble("quantity"));
+                material.setTransportCost(resultSet.getDouble("transportcost"));
+                material.setCoefficientQuality(resultSet.getDouble("qualitycoefficient"));
+                return Optional.of(material);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Material> findAll() {
+        String query = "SELECT * FROM materials";
+        List<Material> materials = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Material material = new Material();
+                material.setName(resultSet.getString("name"));
+                material.setVatRate(resultSet.getDouble("vatrate"));;
+                material.setUnitCost(resultSet.getDouble("unitcost"));
+                material.setQuantity(resultSet.getDouble("quantity"));
+                material.setTransportCost(resultSet.getDouble("transportcost"));
+                material.setCoefficientQuality(resultSet.getDouble("qualitycoefficient"));
+                materials.add(material);
+                return materials;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
     public Material update(Material material) {
-        return null;
+        String query = "UPDATE materials SET name = ?, vatrate = ?, project_id = ?, unitcost = ?, quantity = ?, transportcost = ?, qualitycoefficient = ? WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, material.getName());
+            statement.setDouble(2, material.getVatRate());
+            statement.setInt(3, material.getProject().getId());
+            statement.setDouble(4, material.getUnitCost());
+            statement.setDouble(5, material.getQuantity());
+            statement.setDouble(6, material.getTransportCost());
+            statement.setDouble(7, material.getCoefficientQuality());
+            statement.setInt(8, material.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return material;
     }
 
     @Override
