07008e46c06e96df5c307caa42c0ef34c256d859
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-09-21 17:11:57 +0100

    lier project avec material

--- src/BatiCuisine/Views/ProjectView.java
+++ src/BatiCuisine/Views/ProjectView.java
@@ -1,6 +1,7 @@
 package BatiCuisine.Views;
 
 import BatiCuisine.Entities.Client;
+import BatiCuisine.Entities.Material;
 import BatiCuisine.Entities.Project;
 import BatiCuisine.Enums.ProjectStatus;
 import BatiCuisine.Services.ProjectService;
@@ -10,12 +11,14 @@ import java.util.Scanner;
 public class ProjectView {
     private ProjectService projectService;
     private LaborView laborView ;
+    private MaterialView materialView;
     Scanner scanner = new Scanner(System.in);
 
     public ProjectView() {
 
         this.projectService = projectService;
         this.laborView = laborView;
+        this.materialView = materialView;
     }
     public void saveProjectForClient(Client client){
         System.out.println("-----Création d'un Nouveau Proje----");
@@ -23,6 +26,7 @@ public class ProjectView {
         String name =scanner.nextLine();
         Project project = new Project(name, 0.0,0.0, ProjectStatus.INPROGRESS,client);
         Project savedProject = projectService.save(project);
+        materialView.createMaterial(savedProject);
         laborView.creatLabor(savedProject);
 
 
