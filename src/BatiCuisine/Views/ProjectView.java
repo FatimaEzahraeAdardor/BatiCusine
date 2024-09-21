6d75e1bc7b7d43a91894ae4a866da9d3dea1fdb0
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-09-21 16:43:29 +0100

    lier project avec labor

--- src/BatiCuisine/Views/ProjectView.java
+++ src/BatiCuisine/Views/ProjectView.java
@@ -9,17 +9,22 @@ import java.util.Scanner;
 
 public class ProjectView {
     private ProjectService projectService;
+    private LaborView laborView ;
     Scanner scanner = new Scanner(System.in);
 
     public ProjectView() {
+
         this.projectService = projectService;
+        this.laborView = laborView;
     }
     public void saveProjectForClient(Client client){
         System.out.println("-----Création d'un Nouveau Proje----");
         System.out.println("Entrer nom de projet");
         String name =scanner.nextLine();
         Project project = new Project(name, 0.0,0.0, ProjectStatus.INPROGRESS,client);
-        Project saveProject = projectService.save(project);
+        Project savedProject = projectService.save(project);
+        laborView.creatLabor(savedProject);
+
 
     }
 }
