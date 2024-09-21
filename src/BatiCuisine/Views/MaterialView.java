70e2a866dc98f537942b701ae5ca5c5919e08d02
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-09-21 17:10:20 +0100

    ajouter methode createMaterial en materialView

---
+++ src/BatiCuisine/Views/MaterialView.java
@@ -0,0 +1,37 @@
+package BatiCuisine.Views;
+
+import BatiCuisine.Entities.Material;
+import BatiCuisine.Entities.Project;
+import BatiCuisine.Repository.Implementation.MaterialRepository;
+
+import java.util.Scanner;
+
+public class MaterialView {
+    private MaterialRepository materialRepository;
+    Scanner scanner = new Scanner(System.in);
+    public MaterialView() {
+        this.materialRepository = materialRepository;
+    }
+    public void createMaterial(Project project){
+        System.out.println("--- Ajout des matériaux ---");
+        do {
+            System.out.println("Entrez le nom du matériau : ");
+            String name = scanner.nextLine();
+            System.out.println("Entrez la quantité de ce matériau (en m²) : ");
+            Double quantity = scanner.nextDouble();
+            System.out.println("Entrez le coût unitaire de ce matériau (€/m²) : ");
+            Double unitCost = scanner.nextDouble();
+            System.out.println("Entrez le coût de transport de ce matériau (€) : ");
+            Double transportCost = scanner.nextDouble();
+            System.out.println("Entrez le coefficient de qualité du matériau (1.0 = standard, > 1.0 = haute qualité) : ");
+            Double qualityCoefficient = scanner.nextDouble();
+            scanner.nextLine();
+            Material material = new Material(name, "Material",0.20, project, unitCost, quantity, transportCost, qualityCoefficient);
+            materialRepository.save(material);
+            System.out.println("Matériau ajouté avec succès !");
+            System.out.println("Voulez-vous ajouter un autre matériau ? (y/n) ");
+
+        }while (scanner.nextLine().equalsIgnoreCase("y"));
+
+    }
+}
