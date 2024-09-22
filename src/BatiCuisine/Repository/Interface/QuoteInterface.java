3c24dd87b19a709e42c3501131b2ecfa0bbda299
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-09-22 21:34:31 +0100

    ajouter quoteInterface

---
+++ src/BatiCuisine/Repository/Interface/QuoteInterface.java
@@ -0,0 +1,19 @@
+package BatiCuisine.Repository.Interface;
+
+import BatiCuisine.Entities.Client;
+import BatiCuisine.Entities.Quote;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuoteInterface {
+    Quote save(Quote quote) ;
+
+    Optional<Quote> findById(int id);
+
+    List<Quote> findAll() ;
+
+    Quote update(Quote quote);
+
+    Boolean delete(int id);
+}
